895b1b57f1d175f9669a31874a1a3d7635fcc97d
BatuSengezer <bsengezer@ku.edu.tr>
2023-05-22 17:58:04 +0200

    data processing

--- data_exploration.py
+++ data_exploration.py
@@ -3,28 +3,24 @@ import pandas as pd
 # # Reading from files(csv, xlsx, tsv)
 price_file = "raw_data/airbnb_price.csv"
 prices = pd.read_csv(price_file)
-# print(prices.head())
 
 xls = pd.ExcelFile("raw_data/airbnb_room_type.xlsx")
 rooms = pd.read_excel(xls)
-# print(rooms.head())
 
 last_review = pd.read_csv("raw_data/airbnb_last_review.tsv", sep= '\t')
-# print(last_review.head())
 
 # # Cleaning price column
+
 # Removing extra string
 prices["price"] = prices["price"].str.replace(" dollars", "")
-# print(prices.head())
 
 # converting object to numeric
-# print(prices["price"].dtype)
 prices["price"]= pd.to_numeric(prices["price"])
-# print(prices["price"].dtype)
 
 # # Calculating average price
+
 # checking outliers in price
-#print(prices.sort_values(by = 'price'))
+# print(prices.sort_values(by = 'price'))
 
 # filtering outliers 
 filtered_prices = prices.query("price != 0 & price != 7500")
@@ -32,4 +28,26 @@ filtered_prices = prices.query("price != 0 & price != 7500")
 
 # calculating average
 avg_price = round(filtered_prices["price"].mean(),2)
-# print(avg_price)
+
+# # Comparing costs to the private rental market
+
+# adding price per month column to prices
+prices["price_per_month"] = round(prices["price"]*365/12,0).astype(int)
+
+# calculating the average price of the price_per_month
+average_price_per_month = round(prices["price_per_month"].mean(),2)
+
+# calculating difference between the avg cost of Airbnb listing versus the private market
+# according to Zumper, a 1 bedroom apartment in New York City costs, on average, $3,100 per month
+difference = round(average_price_per_month - 3100,2)
+
+# # Cleaning the room_type column
+
+# changing all values in the room_type column to lowercase
+rooms["room_type"] = rooms["room_type"].str.lower()
+
+# converting the room_type column to a str dtype
+rooms["room_type"] = rooms["room_type"].astype('string')
+
+# storing the count of values for room_type as room_frequencies
+room_frequencies = rooms["room_type"].value_counts().to_string()
