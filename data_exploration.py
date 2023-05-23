79ee8b94cc11253e20f9e5d8a89056ab2219e0a1
BatuSengezer <bsengezer@ku.edu.tr>
2023-05-23 14:24:17 +0200

    ongoing

--- data_exploration.py
+++ data_exploration.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 # # Reading from files(csv, xlsx, tsv)
 price_file = "raw_data/airbnb_price.csv"
@@ -22,24 +23,24 @@ prices["price"]= pd.to_numeric(prices["price"])
 # checking outliers in price
 # print(prices.sort_values(by = 'price'))
 
-# filtering outliers 
-filtered_prices = prices.query("price != 0 & price != 7500")
-# print(filtered_prices.describe())
+# filtering outliers (free listings)
+prices = prices.query("price != 0")
+# print(prices.describe())
 
 # calculating average
-avg_price = round(filtered_prices["price"].mean(),2)
+avg_price = round(prices["price"].mean(),2)
 
 # # Comparing costs to the private rental market
 
 # adding price per month column to prices
-prices["price_per_month"] = round(prices["price"]*365/12,0).astype(int)
+prices["price_per_month"] = prices["price"]*365/12
 
 # calculating the average price of the price_per_month
 average_price_per_month = round(prices["price_per_month"].mean(),2)
 
 # calculating difference between the avg cost of Airbnb listing versus the private market
 # according to Zumper, a 1 bedroom apartment in New York City costs, on average, $3,100 per month
-difference = round(average_price_per_month - 3100,2)
+difference = round((average_price_per_month - 3100),2)
 
 # # Cleaning the room_type column
 
@@ -50,7 +51,7 @@ rooms["room_type"] = rooms["room_type"].str.lower()
 rooms["room_type"] = rooms["room_type"].astype('string')
 
 # storing the count of values for room_type as room_frequencies
-room_frequencies = rooms["room_type"].value_counts().to_string()
+room_frequencies = rooms["room_type"].value_counts()
 
 # # What timeframe are we working with?
 
@@ -64,9 +65,9 @@ last_reviewed = reviews["last_review"].max()
 # # Joining the DataFrames
 
 # outer merging dataframes
-rooms_and_prices = pd.merge(prices, rooms, on="listing_id")
+rooms_and_prices = pd.merge(prices, rooms, how = 'outer', on="listing_id")
 
-airbnb_merged = pd.merge(rooms_and_prices, reviews, on = "listing_id")
+airbnb_merged = pd.merge(rooms_and_prices, reviews, how = 'outer', on = "listing_id")
 # print(airbnb_merged.columns)
 # print(airbnb_merged[airbnb_merged.isnull().any(axis=1)])
 
@@ -76,4 +77,38 @@ airbnb_merged.dropna(inplace=True)
 # checking for duplicate values in airbnb_merged
 # print(airbnb_merged.duplicated().sum())
 
-# Analyzing listing prices by NYC borough
+# # Analyzing listing prices by NYC borough
+
+# creating a new column in airbnb_merged called borough
+airbnb_merged["borough"] = airbnb_merged["nbhood_full"].str.partition(",")[0]
+
+# grouping airbnb_merged by borough and calculating summary statistics
+boroughs = airbnb_merged.groupby("borough")["price"].agg(["sum", "mean", "median", "count"])
+
+# updating the boroughs DataFrame
+boroughs = boroughs.round(2).sort_values(by = "mean", ascending= False)
+# print(boroughs)
+
+# # Price range by borough
+
+# creating labels list
+label_names = ["Budget", "Average", "Expensive", "Extravagant"]
+
+# creating ranges list
+ranges = [0, 69, 175, 350, np.inf]
+
+# creating price range column
+airbnb_merged["price_range"] = pd.cut(airbnb_merged["price"], bins = ranges, labels = label_names)
+
+# calculating count of price ranges
+prices_by_borough = airbnb_merged.groupby(by = ["borough", "price_range"])["price_range"].count()
+# Storing the final result
+
+airbnb_analysis = {'avg_price': avg_price,
+                   'average_price_per_month': average_price_per_month,
+                   'difference': difference,
+                   'room_frequencies': room_frequencies,
+                   'first_reviewed': first_reviewed,
+                   'last_reviewed': last_reviewed,
+                   'prices_by_borough': prices_by_borough}
+print(airbnb_analysis)
